2d6f39d649a8555a4ff27f229878887548c60782
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-11-03 11:43:41 -0600

    finished built in types sub-section. it was crazy to find out the strange behavior of null and that functions have a .length property that is determined by the number of parameters it has. i knew that functions were objects already but i had no idea about the .length prop. super cool chapter so far

---
+++ script.js
@@ -0,0 +1,49 @@
+/*
+CHAPTER 1: TYPES
+
+  The ECMAScript language types are Undefined, Null, Boolean, String, Number, and Object.
+
+A Type By Any Other Name...
+
+  Armed with a full understanding of JavaScript types, we're aiming to illustrate why coercion's bad reputation is largely overhyped and somewhat undeserved -- to flip your perspective, to seeing coercion's power and usefulness.
+
+Built-in Types
+
+  JavaScript defines seven built-in types:
+
+  null
+  undefined
+  boolean
+  number
+  string
+  object
+  symbol -- added in ES6!
+  Note: All of these types except object are called "primitives".
+
+  The typeof operator inspects the type of the given value, and always returns one of seven string values -- surprisingly, there's not an exact 1-to-1 match with the seven built-in types we just listed.
+  typeof undefined     === "undefined"; // true
+  typeof true          === "boolean";   // true
+  typeof 42            === "number";    // true
+  typeof "42"          === "string";    // true
+  typeof { life: 42 }  === "object";    // true
+
+  // added in ES6!
+  typeof Symbol()      === "symbol";    // true
+
+  As you may have noticed, I excluded null from the above listing. It's special -- special in the sense that it's buggy when combined with the typeof operator:
+
+  typeof null === "object"; // true
+
+  null is the only primitive value that is "falsy" (aka false-like; see Chapter 4) but that also returns "object" from the typeof check.
+
+  It's easy to think that function would be a top-level built-in type in JS, However, if you read the spec, you'll see it's actually a "subtype" of object. Specifically, a function is referred to as a "callable object" -- an object that has an internal [[Call]] property that allows it to be invoked.
+
+
+
+
+
+
+
+
+
+*/
