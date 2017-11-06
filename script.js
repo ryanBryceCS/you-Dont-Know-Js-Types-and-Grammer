ef0d869c3f85e35ec717475b7b357e0df99082ea
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-11-05 23:32:44 -0700

    finished chapter one of types and gramar. learned more about the typeof operator then i knew was possilbe and how to use the type of opperator to check for undeclared vars vs undefined vars.

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ PREFACE:
 
   The JavaScript you know right now is probably parts handed down to you by others who've been burned by incomplete understanding. That JavaScript is but a shadow of the true language. You don't really know JavaScript, yet, but if you dig into this series, you will. Read on, my friends. JavaScript awaits you.
 
+
 CHAPTER 1: TYPES
 
   The ECMAScript language types are Undefined, Null, Boolean, String, Number, and Object.
@@ -17,6 +18,7 @@ CHAPTER 1: TYPES
 A Type By Any Other Name...
 
   Armed with a full understanding of JavaScript types, we're aiming to illustrate why coercion's bad reputation is largely overhyped and somewhat undeserved -- to flip your perspective, to seeing coercion's power and usefulness.
+
 
 Built-in Types
 
@@ -51,18 +53,112 @@ Built-in Types
 
   The fact that functions are actually objects is quite useful. Most importantly, they can have properties. For example:
 
-function a(b,c) {
+  function a(b,c) {
 
-}
-The function object has a length property set to the number of formal parameters it is declared with.
+  }
+  The function object has a length property set to the number of formal parameters it is declared with.
 
-a.length; // 2
-Since you declared the function with two formal named parameters (b and c), the "length of the function" is 2.
+  a.length; // 2
+  Since you declared the function with two formal named parameters (b and c), the "length of the function" is 2.
 
-What about arrays? They're native to JS, so are they a special type?
+  What about arrays? They're native to JS, so are they a special type?
 
-typeof [1,2,3] === "object"; // true
-Nope, just objects. It's most appropriate to think of them also as a "subtype" of object (see Chapter 3), in this case with the additional characteristics of being numerically indexed (as opposed to just being string-keyed like plain objects) and maintaining an automatically updated .length property.
+  typeof [1,2,3] === "object"; // true
+  Nope, just objects. It's most appropriate to think of them also as a "subtype" of object (see Chapter 3), in this case with the additional characteristics of being numerically indexed (as opposed to just being string-keyed like plain objects) and maintaining an automatically updated .length property.
+
+
+//Values as Types
+
+  In JavaScript, variables don't have types -- values have types. Variables can hold any value, at any time.
+
+  you use typeof against a variable, it's not asking "what's the type of the variable?" as it may seem, since JS variables have no types. Instead, it's asking "what's the type of the value in the variable?"
+
+  The typeof operator always returns a string. So:
+
+  typeof typeof 42; // "string"
+  The first typeof 42 returns "number", and typeof "number" is "string".
+
+  An "undefined" variable is one that has been declared in the accessible scope, but at the moment has no other value in it. By contrast, an "undeclared" variable is one that has not been formally declared in the accessible scope.
+  //   var a;
+  //   function hmm() {
+  //   var f;
+  //   console.log(a); //a is undefined availiable in acessible scope
+  // }
+  // hmm();
+  // console.log(f); //f is not defined
+
+  There's also a special behavior associated with typeof as it relates to undeclared variables that even further reinforces the confusion. Consider:
+
+  The typeof operator returns "undefined" even for "undeclared" (or "not defined") variables. Notice that there was no error thrown when we executed typeof b, even though b is an undeclared variable. This is a special safety guard in the behavior of typeof.
+
+  As a simple example, imagine having a "debug mode" in your program that is controlled by a global variable (flag) called DEBUG. You'd want to check if that variable was declared before performing a debug task like logging a message to the console. A top-level global var DEBUG = true declaration would only be included in a "debug.js" file, which you only load into the browser when you're in development/testing, but not in production.
+
+  However, you have to take care in how you check for the global DEBUG variable in the rest of your application code, so that you don't throw a ReferenceError. The safety guard on typeof is our friend in this case.
+
+  // oops, this would throw an error!
+  if (DEBUG) {
+  	console.log( "Debugging is starting" );
+  }
+
+  // this is a safe existence check
+  if (typeof DEBUG !== "undefined") {
+  	console.log( "Debugging is starting" );
+  }
+
+  This sort of check is useful even if you're not dealing with user-defined variables (like DEBUG). If you are doing a feature check for a built-in API, you may also find it helpful to check without throwing an error:
+
+  // if (typeof atob === "undefined") {
+  // 	atob = function() {};
+  // }
+
+  Another way of doing these checks against global variables but without the safety guard feature of typeof is to observe that all global variables are also properties of the global object, which in the browser is basically the window object. So, the above checks could have been done (quite safely) as:
+  if (window.DEBUG) {
+	// ..
+  }
+
+  if (!window.atob) {
+  	// ..
+  }
+  Other developers would prefer a design pattern called "dependency injection," where instead of doSomethingCool() inspecting implicitly for FeatureXYZ to be defined outside/around it, it would need to have the dependency explicitly passed in, like:
+
+  function doSomethingCool(FeatureXYZ) {
+  	var helper = FeatureXYZ ||
+  		function() { /.. default feature ../ };
+
+  	var val = helper();
+  	// ..
+  }
+
+  JavaScript has seven built-in types: null, undefined, boolean, number, string, object, symbol. They can be identified by the typeof operator.
+
+  Variables don't have types, but the values in them do. These types define intrinsic behavior of the values.
+
+  Many developers will assume "undefined" and "undeclared" are roughly the same thing, but in JavaScript, they're quite different. undefined is a value that a declared variable can hold. "Undeclared" means a variable has never been declared.
+
+  JavaScript unfortunately kind of conflates these two terms, not only in its error messages ("ReferenceError: a is not defined") but also in the return values of typeof, which is "undefined" for both cases.
+
+  However, the safety guard (preventing an error) on typeof when used against an undeclared variable can be helpful in certain cases.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
